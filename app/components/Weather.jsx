caf3bb2d78888bab59ec0382a48ac572e38a0e81
Anuj Verma <anuj@joshsoftware.com>
2017-02-19 01:35:30 +0530

    Styles weather page

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -31,14 +31,14 @@ var Weather = React.createClass({
     var {isLoading, temp, location} = this.state;
     function renderMessage() {
       if (isLoading) {
-        return <h3> Fetching weather .. </h3>;
+        return <h3 className="text-center"> Fetching weather .. </h3>;
       } else if (temp && location) {
         return <WeatherMessage temp={temp} location={location}/>;
       }
     }
     return (
         <div>
-          <h1> Get Weather </h1>
+          <h1 className="text-center"> Get Weather </h1>
           <WeatherForm onSearch={this.handleSearch}/>
           {renderMessage()}
         </div>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,18 +1,8 @@
 var React = require('react');
-/*
-var WeatherMessage = React.createClass({
-  render: function() {
-    var {temp, location} = this.props;
-    return (
-        <h1>It's {temp} in {location}</h1>
-    );
-  }
-});
-*/
 
 var WeatherMessage = ({temp, location}) => {
   return (
-      <h1>It's {temp} in {location}</h1>
+      <h3 className="text-center">It's {temp} in {location}</h3>
   );
 }
 module.exports = WeatherMessage;
