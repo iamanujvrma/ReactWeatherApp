07bf3b5c44b5f3ff40fb88de28463b242a442878
Anuj Verma <anuj@joshsoftware.com>
2017-02-19 01:29:58 +0530

    Styles About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,21 +1,23 @@
 var React = require('react');
-/*
-var About = React.createClass({
-  render: function () {
-    return (
-        <div>
-          <h3>About Page</h3>
-          <p>Welcome to about Page</p>
-        </div>
-    );
-  }
-});
-*/
+
 var About = (props) => {
   return (
     <div>
-      <h3>About Page</h3>
-      <p>Welcome to about Page</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application build on React.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript library used
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   )
 };
